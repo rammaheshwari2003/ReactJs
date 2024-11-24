a5c3ba1374ed7e30aa77f91edb50ce99eed53203
Ram Maheshwari <rammaheshwari2022@gmail.com>
2024-11-24 19:42:48 +0530

    21 Nov

---
+++ November/21 Nov/src/Layout.jsx
@@ -0,0 +1,20 @@
+import {Link, Outlet}  from "react-router-dom";
+
+const Layout=()=>{
+    return(
+        <>
+          <Link to="home">Home</Link> |
+          <Link to="insert">Insert</Link> |
+          <Link to="display">Display</Link>
+          <hr size="4" color="red" />
+
+          <Outlet/>
+
+ <br/>
+          www.mycompany.com 
+        
+        </>
+    )
+}
+
+export default Layout;
