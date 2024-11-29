2bdb10b327c4af2183d2dc955ac2405c4ee40c49
Ram Maheshwari <rammaheshwari2022@gmail.com>
2024-11-29 20:20:51 +0530

    29 Nov

--- November/28 Nov CRUD/src/Pages/Search.jsx
+++ November/28 Nov CRUD/src/Pages/Search.jsx
@@ -1,7 +1,56 @@
+import { Table } from "react-bootstrap";
+import { useState } from "react";
+import axios from "axios";
+import { message } from "antd";
+
 const Search=()=>{
+    const [year, setYear]=useState("");
+    const [mydata, setMydata]=useState([]);
+    const [errMsg, seterrMsg]=useState("");
+    const handleSubmit=()=>{
+        let api=`http://localhost:3000/Books/?publish_year=${year}`;
+        axios.get(api).then((res)=>{
+            setMydata(res.data);
+            if(res.data.length<=0){
+                errMsg=message.error("No books for this year ");
+            }else{
+                errMsg="";
+            }
+        })
+    }
+    const ans=mydata.map((key)=>{
+        return(
+            <>
+            <tr>
+                <td>{key.name}</td>
+                <td>{key.price}</td>
+                <td>{key.author_name}</td>
+                <td>{key.publish_year}</td>
+            </tr>
+            </>
+        )
+    });
     return(
         <>
         <h1>Search Books</h1>
+        Enter Publish Year : <input type="text" value={year} onChange={(e)=>{setYear(e.target.value)}} />
+        <button onClick={handleSubmit}>Search</button>
+
+        <hr />
+        <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th> Book name</th>
+          <th>Price</th>
+          <th>Author Name</th>
+          <th>Publish Year</th>
+        </tr>
+      </thead>
+      <tbody>
+    {errMsg}
+      {ans}
+    </tbody>
+    </Table>
         </>
     )
 }
