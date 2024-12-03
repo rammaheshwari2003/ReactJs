c46619179f81d6a5d95b1902ae4585eee7a296e8
Ram Maheshwari <rammaheshwari2022@gmail.com>
2024-12-03 17:51:35 +0530

    25 Nov

--- November/25 Nov/src/App.jsx
+++ November/25 Nov/src/App.jsx
@@ -1,17 +1,20 @@
-import { useState, createContext } from "react";
-import Cybrom from "./Cybrom";
-const myContext=createContext();
-const App=()=>{
-    const [user, setUser]=useState("Ram");
-    return(
-        <>
-            <h1>Welcome to My App Myname : {user}</h1>
-            <button onClick={()=>{setUser("Roshan")}}>Click Here</button>
-            <myContext.Provider value={{user, setUser}}>
-                <Cybrom />
-            </myContext.Provider>
-        </>
-    )
-}
-export default App;
-export {myContext};
+// import { useState, createContext } from "react";
+// import Cybrom from "./Cybrom";
+// const myContext=createContext();
+// const App=()=>{
+//     const [user, setUser]=useState("Ram");
+//     return(
+//         <>
+//             <h1>Welcome to My App Myname : {user}</h1>
+//             <button onClick={()=>{setUser("Roshan")}}>Click Here</button>
+//             <myContext.Provider value={{user, setUser}}>
+//                 <Cybrom />
+//             </myContext.Provider>
+//         </>
+//     )
+// }
+// export default App;
+// export {myContext};
+
+
+
--- November/25 Nov/src/Cybrom.jsx
+++ November/25 Nov/src/Cybrom.jsx
@@ -1,10 +1,11 @@
-import Bhopal from "./Bhopal";
-const Cybrom=()=>{
-    return(
-        <>
-        <h1>Welcome to Cybrom</h1>
-        <Bhopal />
-        </>
-    )
-}
-export default Cybrom;
+// import Bhopal from "./Bhopal";
+// const Cybrom=()=>{
+//     return(
+//         <>
+//         <h1>Welcome to Cybrom</h1>
+//         <Bhopal />
+//         </>
+//     )
+// }
+// export default Cybrom;
+
